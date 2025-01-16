4d217d7f650bef613fe443a9e779c58ccab1f74d
Michael Mor Yosef <167459847+michaelmory@users.noreply.github.com>
2025-01-16 14:42:02 +0200

    Delete shredRRMSE.m

--- shredRRMSE.m
+++
@@ -1,9 +0,0 @@
-function [ RRMSE ] = shredRRMSE( GT,Rec )
-%SHREDRRMSE Summary of this function goes here
-%   Detailed explanation goes here
-
-err_map  = sqrt((GT(:)-Rec(:)).^2);
-rel_err  = err_map(:)./GT(:);
-RRMSE = mean(rel_err);
-end
-
